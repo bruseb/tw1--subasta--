0125f588f21aa44f9c6c070723f935a2116460d3
Agustin Pucci <agustin.pucci.rubio@gmail.com>
2025-10-29 23:47:10 -0300

    JSON para obtener lista de ofertas de usuario

--- src/main/java/com/tallerwebi/dominio/ServicioOferta.java
+++ src/main/java/com/tallerwebi/dominio/ServicioOferta.java
@@ -1,5 +1,8 @@
 package com.tallerwebi.dominio;
 
+import java.util.List;
+
 public interface ServicioOferta {
     Oferta ofertar( Long id, String emailCreador, Float montoOfertado);
+    List<Oferta> listarOfertasSubasta(Long idSubasta);
 }
--- src/main/java/com/tallerwebi/infraestructura/RepositorioOfertaImpl.java
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioOfertaImpl.java
@@ -2,9 +2,12 @@ package com.tallerwebi.infraestructura;
 
 import com.tallerwebi.dominio.Oferta;
 import com.tallerwebi.dominio.RepositorioOferta;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository("repositorioOferta")
 public class RepositorioOfertaImpl implements RepositorioOferta {
@@ -19,5 +22,19 @@ public class RepositorioOfertaImpl implements RepositorioOferta {
     @Override
     public void guardarOferta(Oferta oferta) {
         sessionFactory.getCurrentSession().save(oferta);
+    }
+
+    @Override
+    public List<Oferta> obtenerOfertaPorSubasta(Long idSubasta) {
+        //final Session session = sessionFactory.getCurrentSession();
+        Session session;
+        try{
+            session = sessionFactory.getCurrentSession();
+        }catch(Exception e){
+            session = sessionFactory.openSession();
+        }
+        return session.createCriteria(Oferta.class)
+                .add(Restrictions.eq("subasta.id", idSubasta))
+                .list();
     }
 }
