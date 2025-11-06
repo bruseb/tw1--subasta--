3c3b8199245fe402c50398b14965e9b072455e31
Bruno <brunosmerlo@gmail.com>
2025-11-06 17:15:00 -0300

    Se corrige las clases de la funcionalidad calcular-envio

---
+++ src/main/java/com/tallerwebi/dominio/Envio.java
@@ -0,0 +1,84 @@
+package com.tallerwebi.dominio;
+
+public class Envio {
+
+    private Double peso;
+    private Double largo;
+    private Double alto;
+    private Double ancho;
+    private String pais;
+    private String provincia;
+    private String ciudad;
+    private Integer codigoPostal;
+    private String tamanio;
+    private Double costo;
+    private Integer diasDeEntrega;
+    private String zonaDestino;
+
+    public Double getPeso() { return peso; }
+    public void setPeso(Double peso) { this.peso = peso; }
+
+    public Double getLargo() { return largo; }
+
+    public void setLargo(Double largo) {
+        this.largo = largo;
+    }
+
+    public Double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(Double alto) {
+        this.alto = alto;
+    }
+
+    public Double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(Double ancho) {
+        this.ancho = ancho;
+    }
+
+    public String getPais() { return pais; }
+    public void setPais(String pais) { this.pais = pais; }
+
+    public String getProvincia() { return provincia; }
+    public void setProvincia(String provincia) { this.provincia = provincia; }
+
+    public String getCiudad() { return ciudad; }
+    public void setCiudad(String ciudad) { this.ciudad = ciudad; }
+
+    public Integer getCodigoPostal() { return codigoPostal; }
+    public void setCodigoPostal(Integer codigoPostal) { this.codigoPostal = codigoPostal; }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public Double getCosto() {
+        return costo;
+    }
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    public Integer getDiasDeEntrega() {
+        return diasDeEntrega;
+    }
+    public void setDiasDeEntrega(Integer diasDeEntrega) {
+        this.diasDeEntrega = diasDeEntrega;
+    }
+
+    public String getZonaDestino() {
+        return zonaDestino;
+    }
+    public void setZonaDestino(String zonaDestino) {
+        this.zonaDestino = zonaDestino;
+    }
+}
+
+
